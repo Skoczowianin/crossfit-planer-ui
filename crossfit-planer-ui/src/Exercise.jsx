dce5450c2ec840db34a3c32f7a49d78423982b54
mikolaj <mikigrzybek6@gmail.com>
2024-05-08 14:55:22 +0200

    Components with parameters

--- crossfit-planer-ui/src/Exercise.jsx
+++ crossfit-planer-ui/src/Exercise.jsx
@@ -2,26 +2,22 @@ import { faker } from '@faker-js/faker';
 import React from 'react'
 import './exercise.css'
 
-const Exercise = () => {
+const Exercise = (props) => {
     function imageAvatar() {
         const randomAvatar = faker.image.avatar();
-        console.log(randomAvatar)
         return randomAvatar;
 
     }
     function description() {
         const randomDescription = faker.word.words();
-        console.log(randomDescription)
         return randomDescription;
     }
     function title() {
         const randomTitle = faker.person.jobTitle();
-        console.log(randomTitle)
         return randomTitle;
     }
     function reps() {
         const randomReps = faker.number.int({min: 1, max: 10})
-        console.log(randomReps)
         return randomReps;
     }
    
@@ -38,11 +34,11 @@ const Exercise = () => {
   return (
     
     <div className='exercise'>
-       <div ><img src={imageAvatar()}></img></div>
-       <div>{description()}</div>
-       <div>{title()}</div>
-        <div>{reps()}</div>
-        <div>{generateType()}</div>
+       <div ><img src={ props.image ? props.image : imageAvatar()}></img></div>
+       <div>{props.title ? props.title : title()}</div>
+       <div>{props.description ? props.description : description()}</div>
+        <div>{props.reps ? props.reps : reps()}</div>
+        <div>{props.type ? props.type : generateType()}</div>
     </div>
   )
 }
--- crossfit-planer-ui/src/Template.jsx
+++ crossfit-planer-ui/src/Template.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import Exercise from './Exercise'
 
 const Template = () => {
+
+
   return (
     <>
     <h2>Miki Training</h2>
     <div>In this page I will show you some exercises</div>
-    <ul>
-        <li>Jumping Jacks</li>
-        <li>Push ups</li>
-        <li>Squats</li>
-        <li>Burpees</li>
-    </ul>
+    <div>
+        <Exercise 
+        image="./src/assets/pushUps.jpg"
+        title="Push Ups"
+        description="asdasda"
+        value={10}
+        type="repetitions"
+        />
+        <Exercise />
+        <Exercise />
+    </div>
     </>
   )
 }
