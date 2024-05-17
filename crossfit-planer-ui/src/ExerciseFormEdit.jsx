139a9c59dbda0f8069afdfc8288a954af7568984
mikolaj <mikigrzybek6@gmail.com>
2024-05-17 11:50:18 +0200

    moving edit exercise form to separate component

---
+++ crossfit-planer-ui/src/ExerciseFormEdit.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./template.css";
+
+const ExerciseFormEdit = ({handleEditSubmit, hideEditForm, currentExercise}) => {
+  return (
+    <div>
+        <div className="adding-exercise">        
+        </div>
+        <form className="form-container" onSubmit={handleEditSubmit}>
+          <input type="text" name="title" placeholder="Title" defaultValue={currentExercise.title} />
+          <br />
+          <input type="text" name="description" placeholder="Description" defaultValue={currentExercise.description}/>
+          <br />
+          <select name="type" defaultValue={currentExercise.type}>
+            <option value="repetitions">Repetitions</option>
+            <option value="seconds">Seconds</option>
+          </select>
+          <br />
+          <input
+            type="number"
+            name="value"
+            defaultValue={currentExercise.value}
+            placeholder="Number of repetitions or seconds"
+            min={1}
+          />
+          <br />
+          <input type="file" name="image" accept="image/*" filename={currentExercise.image} />
+          <br />
+          <br />
+          <div className="button-space">
+            <button type="submit" onSubmit={handleEditSubmit}>Save</button>
+            <button type="button" onClick={hideEditForm}>
+              Cancel
+            </button>
+          </div>
+        </form>
+    </div>
+  )
+}
+
+export default ExerciseFormEdit
--- crossfit-planer-ui/src/Template.jsx
+++ crossfit-planer-ui/src/Template.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Exercise from "./Exercise";
 import ExerciseFormCreate from "./ExerciseFormCreate";
+import ExerciseFormEdit from "./ExerciseFormEdit";
 import "./template.css";
 
 const Template = () => {
@@ -111,36 +112,7 @@ const Template = () => {
 
       {createForm && <ExerciseFormCreate hideCreateForm={hideCreateForm} handleCreateExercise={handleCreateExercise} />}
 
-      {editForm && (
-        <form className="form-container" onSubmit={handleEditSubmit}>
-          <input type="text" name="title" placeholder="Title" defaultValue={currentExercise.title} />
-          <br />
-          <input type="text" name="description" placeholder="Description" defaultValue={currentExercise.description}/>
-          <br />
-          <select name="type" defaultValue={currentExercise.type}>
-            <option value="repetitions">Repetitions</option>
-            <option value="seconds">Seconds</option>
-          </select>
-          <br />
-          <input
-            type="number"
-            name="value"
-            defaultValue={currentExercise.value}
-            placeholder="Number of repetitions or seconds"
-            min={1}
-          />
-          <br />
-          <input type="file" name="image" accept="image/*" filename={currentExercise.image} />
-          <br />
-          <br />
-          <div className="button-space">
-            <button type="submit" onSubmit={handleEditSubmit}>Save</button>
-            <button type="button" onClick={hideEditForm}>
-              Cancel
-            </button>
-          </div>
-        </form>
-      )}
+      {editForm && <ExerciseFormEdit hideEditForm={hideEditForm} handleEditSubmit={handleEditSubmit} currentExercise={currentExercise}/>}
     </div>
   );
 };
