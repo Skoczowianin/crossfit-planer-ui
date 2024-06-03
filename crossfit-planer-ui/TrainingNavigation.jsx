e239b040f159dae2b954b52541ac93626a951358
mikikrol123 <79933739+mikikrol123@users.noreply.github.com>
2024-06-03 20:16:57 +0200

    37 display training status (#40)
    * Created TrainingStatus component

--- crossfit-planer-ui/TrainingNavigation.jsx
+++ crossfit-planer-ui/TrainingNavigation.jsx
@@ -5,13 +5,18 @@ const TrainingNavigation = () => {
     const textPause = "PAUSE TRAINING"
     const textContinue = "CONTINUE TRAINING"
     const textStart = "START TRAINING"
+    const trainingNotStarted = "NOT STARTED"
+    const trainingInProgress = "IN PROGRESS"
+    const trainignPaused = "PAUSED"
     const [startButton, setStartButton] = useState(textStart);
+    const [trainingStatus, setTrainingStatus] = useState(trainingNotStarted)
 
     function startTraining() {
         setStartButton(textPause)
-
+        setTrainingStatus(trainingInProgress)
         if(startButton === textPause) {
             continueTraining();
+            setTrainingStatus(trainignPaused)
         }
     }
     function continueTraining() {
@@ -19,11 +24,15 @@ const TrainingNavigation = () => {
     }
     function stopTraining() {
         setStartButton(textStart)
+        setTrainingStatus(trainingNotStarted)
     }
   return (
     <div className='training-navigation'>
+        <div className='training-buttons'>
         <button onClick={startTraining}>{startButton}</button>
         <button onClick={stopTraining}>STOP TRAINING</button>
+        </div>
+        <div className='training-status'>{trainingStatus}</div>
     </div>
   )
 }
