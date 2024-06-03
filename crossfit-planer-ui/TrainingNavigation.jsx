4e9263fa8aaeee44fe42d3f3a14970f347245ad5
Mikolaj <mikigrzybek6@gmail.com>
2024-06-03 20:48:06 +0200

    Added styles

--- crossfit-planer-ui/TrainingNavigation.jsx
+++ crossfit-planer-ui/TrainingNavigation.jsx
@@ -1,40 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./trainingNavigation.css";
 
 const TrainingNavigation = () => {
-    const textPause = "PAUSE TRAINING"
-    const textContinue = "CONTINUE TRAINING"
-    const textStart = "START TRAINING"
-    const trainingNotStarted = "NOT STARTED"
-    const trainingInProgress = "IN PROGRESS"
-    const trainignPaused = "PAUSED"
-    const [startButton, setStartButton] = useState(textStart);
-    const [trainingStatus, setTrainingStatus] = useState(trainingNotStarted)
+  const textPause = "PAUSE TRAINING";
+  const textContinue = "CONTINUE TRAINING";
+  const textStart = "START TRAINING";
+  const trainingNotStarted = "NOT STARTED";
+  const trainingInProgress = "IN PROGRESS";
+  const trainignPaused = "PAUSED";
+  const [startButton, setStartButton] = useState(textStart);
+  const [trainingStatus, setTrainingStatus] = useState(trainingNotStarted);
 
-    function startTraining() {
-        setStartButton(textPause)
-        setTrainingStatus(trainingInProgress)
-        if(startButton === textPause) {
-            continueTraining();
-            setTrainingStatus(trainignPaused)
-        }
+  function startTraining() {
+    setStartButton(textPause);
+    setTrainingStatus(trainingInProgress);
+    if (startButton === textPause) {
+      continueTraining();
+      setTrainingStatus(trainignPaused);
     }
-    function continueTraining() {
-        setStartButton(textContinue)
-    }
-    function stopTraining() {
-        setStartButton(textStart)
-        setTrainingStatus(trainingNotStarted)
-    }
+  }
+  function continueTraining() {
+    setStartButton(textContinue);
+  }
+  function stopTraining() {
+    setStartButton(textStart);
+    setTrainingStatus(trainingNotStarted);
+  }
   return (
-    <div className='training-navigation'>
-        <div className='training-buttons'>
+    <div className="training-navigation">
+      <div className="training-buttons">
         <button onClick={startTraining}>{startButton}</button>
         <button onClick={stopTraining}>STOP TRAINING</button>
-        </div>
-        <div className='training-status'>{trainingStatus}</div>
+      </div>
+      <div className="training-status">{trainingStatus}</div>
     </div>
-  )
-}
+  );
+};
 
 export default TrainingNavigation;
